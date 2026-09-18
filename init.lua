e0c5db713dca3effa268db2fcd2770e24eba829d
Moritz Reiter <mail@moritzreiter.de>
2026-09-18 17:10:48 +0200

    Show percentage in statusline

--- init.lua
+++ init.lua
@@ -395,7 +395,7 @@ do
   -- default behavior. For example, here we set the section for
   -- cursor location to LINE:COLUMN
   ---@diagnostic disable-next-line: duplicate-set-field
-  statusline.section_location = function() return '%2l:%-2v' end
+  statusline.section_location = function() return '%2l:%-2v %P' end
 
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
