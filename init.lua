43de013302fb26ce944b52ad533df51c87e659dd
Moritz Reiter <mail@moritzreiter.de>
2026-09-21 15:22:07 +0200

    Configure indentation

--- init.lua
+++ init.lua
@@ -82,6 +82,13 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- Indentation
+  vim.opt.tabstop = 4
+  vim.opt.softtabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.expandtab = true
+  vim.opt.smartindent = true
 end
 
 -- ============================================================
